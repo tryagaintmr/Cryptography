033da981caae5fed9506565f305a0bafcaac20e1
Mohamed Mustapha <chezmohamed@msn.com>
2023-10-13 20:49:21 +0200

    Add project files.

---
+++ Cryptography/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+
+string message = "Hello, World!";
+byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+// 1. Generate RSA Key Pair
+using (var rsa = new RSACryptoServiceProvider())
+{
+    // 2. Create a Hash
+    byte[] hash;
+    using (var sha256 = SHA256.Create())
+    {
+        hash = sha256.ComputeHash(messageBytes);
+    }
+
+    // 3. Sign the hash
+    byte[] signature = rsa.SignHash(hash, CryptoConfig.MapNameToOID("SHA256"));
+
+    // 4. Verify the Signature
+    bool isVerified = rsa.VerifyHash(hash, CryptoConfig.MapNameToOID("SHA256"), signature);
+
+    Console.WriteLine($"Message: {message}");
+    Console.WriteLine($"Signature: {Convert.ToBase64String(signature)}");
+    Console.WriteLine($"Verification Result: {isVerified}");
+
+}
