af91b201a1769390321c689577e94fb2364e75e5
Mohamed Mustapha <chezmohamed@msn.com>
2023-10-18 11:08:02 +0200

    added file signing and encryption

---
+++ Cryptography.FileEncryption/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+Console.WriteLine("Welcome to File Encryption/Decryption Console App!");
+Console.WriteLine("Choose an option:");
+Console.WriteLine("1. Encrypt a file");
+Console.WriteLine("2. Decrypt a file");
+Console.Write("Enter your choice (1/2): ");
+
+char choice = Console.ReadKey().KeyChar;
+Console.WriteLine(); // Move to the next line
+
+switch (choice)
+{
+    case '1':
+        EncryptFile();
+        break;
+    case '2':
+        DecryptFile();
+        break;
+    default:
+        Console.WriteLine("Invalid choice. Please enter '1' to encrypt or '2' to decrypt.");
+        break;
+}
+
+static void EncryptFile()
+{
+    Console.Write("Enter the path of the file to encrypt: ");
+    string filePath = Console.ReadLine();
+
+    using (Aes aesAlg = Aes.Create())
+    {
+        // Generate a random encryption key
+        aesAlg.GenerateKey();
+        byte[] encryptionKey = aesAlg.Key;
+
+        // Display the encryption key (you may want to store it securely)
+        Console.WriteLine("Encryption Key:");
+        Console.WriteLine(BitConverter.ToString(encryptionKey).Replace("-", ""));
+
+        // Encrypt the file
+        byte[] encryptedBytes = Encrypt(filePath, aesAlg);
+
+        // Save the encrypted data to a new file
+        string encryptedFilePath = filePath + ".encrypted";
+        File.WriteAllBytes(encryptedFilePath, encryptedBytes);
+        Console.WriteLine($"File encrypted and saved as {encryptedFilePath}");
+    }
+}
+
+static byte[] Encrypt(string filePath, SymmetricAlgorithm symmetricAlgorithm)
+{
+    byte[] encryptedBytes;
+
+    using (FileStream fsInput = new FileStream(filePath, FileMode.Open))
+    using (MemoryStream msOutput = new MemoryStream())
+    using (CryptoStream cryptoStream = new CryptoStream(msOutput, symmetricAlgorithm.CreateEncryptor(), CryptoStreamMode.Write))
+    {
+        fsInput.CopyTo(cryptoStream);
+        cryptoStream.FlushFinalBlock();
+        encryptedBytes = msOutput.ToArray();
+    }
+
+    return encryptedBytes;
+}
+
+static void DecryptFile()
+{
+    Console.Write("Enter the path of the encrypted file: ");
+    string encryptedFilePath = Console.ReadLine();
+
+    Console.Write("Enter the encryption key (hexadecimal format): ");
+    string keyHex = Console.ReadLine();
+
+    if (!string.IsNullOrEmpty(keyHex) && keyHex.Length == 64)
+    {
+        byte[] encryptionKey = new byte[32];
+        for (int i = 0; i < 32; i++)
+        {
+            encryptionKey[i] = Convert.ToByte(keyHex.Substring(i * 2, 2), 16);
+        }
+
+        Decrypt(encryptedFilePath, encryptionKey);
+    }
+    else
+    {
+        Console.WriteLine("Invalid key format. Please enter a 32-byte hexadecimal key.");
+    }
+}
+
+static void Decrypt(string encryptedFilePath, byte[] encryptionKey)
+{
+    using (Aes aesAlg = Aes.Create())
+    {
+        aesAlg.Key = encryptionKey;
+        byte[] decryptedBytes = DecryptF(encryptedFilePath, aesAlg);
+
+        // Save the decrypted data to a new file
+        string decryptedFilePath = encryptedFilePath + ".decrypted";
+        File.WriteAllBytes(decryptedFilePath, decryptedBytes);
+        Console.WriteLine($"File decrypted and saved as {decryptedFilePath}");
+    }
+}
+
+ static byte[] DecryptF(string encryptedFilePath, SymmetricAlgorithm symmetricAlgorithm)
+{
+    byte[] decryptedBytes;
+
+    using (FileStream fsInput = new FileStream(encryptedFilePath, FileMode.Open))
+    using (MemoryStream msOutput = new MemoryStream())
+    using (CryptoStream cryptoStream = new CryptoStream(msOutput, symmetricAlgorithm.CreateDecryptor(), CryptoStreamMode.Write))
+    {
+        fsInput.CopyTo(cryptoStream);
+        cryptoStream.FlushFinalBlock();
+        decryptedBytes = msOutput.ToArray();
+    }
+
+    return decryptedBytes;
+}
---
+++ Cryptography.FileSigning/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+Console.WriteLine("Digital Signature Console App");
+Console.WriteLine("1. Sign a Document");
+Console.WriteLine("2. Verify a Document Signature");
+Console.Write("Enter your choice (1 or 2): ");
+string choice = Console.ReadLine();
+
+switch (choice)
+{
+    case "1":
+        SignDocument();
+        break;
+    case "2":
+        VerifyDocument();
+        break;
+    default:
+        Console.WriteLine("Invalid choice.");
+        break;
+}
+
+static void SignDocument()
+{
+    try
+    {
+        Console.Write("Enter the path to the document to sign: ");
+        string documentPath = Console.ReadLine();
+
+        Console.Write("Enter the path to the PFX certificate file (or press Enter to generate a self-signed certificate): ");
+        string certificatePath = Console.ReadLine();
+
+        X509Certificate2 certificate;
+
+        if (string.IsNullOrWhiteSpace(certificatePath))
+        {
+            // Generate a self-signed certificate if no certificate path is provided
+            certificate = GenerateSelfSignedCertificate();
+        }
+        else
+        {
+            Console.Write("Enter the password for the certificate: ");
+            string certificatePassword = Console.ReadLine();
+            certificate = new X509Certificate2(certificatePath, certificatePassword);
+        }
+
+        // Sign the document
+        SignDocumentWithRSA(documentPath, certificate);
+
+        Console.WriteLine("Document signed successfully.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error signing the document: " + ex.Message);
+    }
+}
+
+static void VerifyDocument()
+{
+    try
+    {
+        Console.Write("Enter the path to the document to verify: ");
+        string documentPath = Console.ReadLine();
+
+        Console.Write("Enter the path to the PFX certificate file for verification: ");
+        string certificatePath = Console.ReadLine();
+
+        Console.Write("Enter the password for the certificate: ");
+        string certificatePassword = Console.ReadLine();
+
+        X509Certificate2 certificate = new X509Certificate2(certificatePath, certificatePassword);
+
+        // Verify the document's signature
+        bool isVerified = VerifyDocumentSignature(documentPath, certificate);
+
+        Console.WriteLine("Signature Verification: " + (isVerified ? "Successful" : "Failed"));
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error verifying the signature: " + ex.Message);
+    }
+}
+
+static X509Certificate2 GenerateSelfSignedCertificate()
+{
+    // Generate a self-signed certificate for testing purposes
+    using (RSA rsa = RSA.Create(2048))
+    {
+        var request = new CertificateRequest("cn=SelfSignedCertificate", rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        var certificate = request.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(1));
+
+        // Export the certificate to a PFX file (password: "password")
+        File.WriteAllBytes("SelfSignedCertificate.pfx", certificate.Export(X509ContentType.Pfx, "password"));
+
+        return new X509Certificate2("SelfSignedCertificate.pfx", "password");
+    }
+}
+
+static void SignDocumentWithRSA(string documentPath, X509Certificate2 certificate)
+{
+    try
+    {
+        // Load the document to sign
+        string documentContents = File.ReadAllText(documentPath, Encoding.UTF8);
+        byte[] documentBytes = Encoding.UTF8.GetBytes(documentContents);
+
+        // Create a digital signature using the certificate's private key
+        using (RSACryptoServiceProvider rsa = (RSACryptoServiceProvider)certificate.PrivateKey)
+        {
+            byte[] signature = rsa.SignData(documentBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+            // Append the signature to the document
+            File.WriteAllBytes(documentPath + ".sig", signature);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error signing the document: " + ex.Message);
+    }
+}
+
+static bool VerifyDocumentSignature(string documentPath, X509Certificate2 certificate)
+{
+    try
+    {
+        // Load the document and its signature
+        string documentContents = File.ReadAllText(documentPath, Encoding.UTF8);
+        byte[] documentBytes = Encoding.UTF8.GetBytes(documentContents);
+        byte[] signature = File.ReadAllBytes(documentPath + ".sig");
+
+        // Verify the signature using the certificate's public key
+        using (RSACryptoServiceProvider rsa = (RSACryptoServiceProvider)certificate.PublicKey.Key)
+        {
+            return rsa.VerifyData(documentBytes, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error verifying the signature: " + ex.Message);
+        return false;
+    }
+}
